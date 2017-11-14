aec084cc9df4680d10f692d765356db2d45ad294
Esteban Triviño <steven.trivinog@hotmail.com>
2017-11-13 23:21:25 -0500

    Update Rutas.cpp

--- Rutas.cpp
+++ Rutas.cpp
@@ -28,17 +28,6 @@ struct Admin{
 	string password;
 };
 
-struct Reserva{
-	
-	string ruta;
-	string origen;
-	string destino;
-	string identicifacionCliente;
-	//Se sabe si el cliente viaja al extranjero o al interior des pais
-	//verificando el destino.
-	
-}reservas[1000];
-
   // se crea la estuctura de las rutas
 struct Ruta{
  string codigo;
@@ -254,13 +243,7 @@ void comprar(string codigoRuta, Ruta rutas[], cliente cliente1){
 				if(rutas[i].ocupada == rutas[i].cupo){
 					rutas[i].disponibilidad = "false";
 				}
-				
-				//Se crea la reserva
-				int x = 0;
-				while(reservas[x].destino != "\0"){
-					x++;//Aumenta la x
-				}
-				
+								
 				//Actualizar archivo de rutas
 				actualizarRutas(rutas);
 				
@@ -377,6 +360,23 @@ void consultarRuta(Ruta rutas[],cliente cliente1){
 	
 }
 
+bool iniciarAdmin(){
+	
+	string usuario, password;
+	
+	cout<<"INGRESE USUARIO: ";
+	cin>>usuario;
+	cout<<"INGRESE PASSWORD: ";
+	cin>>password;
+	
+	if(usuario == "mafe" && password == "1234"){
+		return true;
+	}else{
+		return false;
+	}
+	
+}
+
 bool iniciarUsuario(cliente clientes[], cliente &c){
 	
 	string usuario, password;
@@ -471,28 +471,49 @@ int buscarcliente(cliente clientes[], string id1){//manda la posici? del element
 	return -1;
 }
 
+void menu2(int &opc){
+	
+	cout<<"1. CONSULTAR RUTAS DISPONIBLES POR ORIGEN-DESTINO\n";
+	cout<<"2. VER DETALLE DE LA RUTA POR ID\n";
+	cout<<"3. COMPRAR TIQUETE\n";
+	cout<<"\nINGRESE UNA OPCION: ";
+	cin>>opc;
+	
+}
+
+void consultarRutaPorId(Ruta rutas[]){
+	string codigo;
+	
+	cout<<"\n\nINGRESE EL CODIGO DE LA RUTA: ";
+	cin>>codigo;
+	
+	int i=0;
+	while(rutas[i].codigo != "\0"){
+		
+		if(rutas[i].codigo == codigo){
+			
+			cout<<rutas[i].codigo<<" "<<rutas[i].rutas<<" "<<rutas[i].origen<<" ";
+			cout<<rutas[i].destino<<" "<<rutas[i].disponibilidad<<" "<<rutas[i].tipo<<" ";
+			cout<<rutas[i].hora<<" "<<rutas[i].cupo<<" "<<rutas[i].disponible<<" ";
+			cout<<rutas[i].ocupada<<" "<<rutas[i].fecha<<" "<<rutas[i].precio<<endl;
+			
+			break;
+		}
+		i++;
+	}
+	
+}
+
 // salida: en la funcion main se llaman las funciones anteriormente declaradas y se imprimen los datos
 int main(){
   	// se declaran la variables 
   	cliente clienteSesion;
-  	int opc=0;
-  	string j;
-  	string nombre;
-  	string apellido;
-  	int id;
-  	string password; 
-  	string origen;
-  	string destino;
-  	string fecha;
-  	ifstream f;
- 	 //char fd;
-  	string us;
+  	int opc=0,opc3=0;
   	
   	bool sesion;
   	
   	cliente clientes[1000];
   	Ruta rutas[1000];
-  	reservas[0].destino = "\0"; //Declaramos la primera reserva nula;
   
   	//administrador a[100];
   	cargardatosc(clientes);
@@ -500,14 +521,6 @@ int main(){
   
   	menu(opc);
   	
-  	//Probar Los clientes
-  	//cout<<clientes[1].Usuario<<" "<<clientes[1].Password<<" "<<clientes[1].Nombre<<" "<<clientes[1].Apellido<<endl;
-  	//Probar las rutas
-	/*cout<<rutas[1].codigo<<" "<<rutas[1].rutas<<" "<<rutas[1].origen<<" ";
-	cout<<rutas[1].destino<<" "<<rutas[1].disponibilidad<<" "<<rutas[1].tipo<<" ";
-	cout<<rutas[1].hora<<" "<<rutas[1].cupo<<" "<<rutas[1].disponible<<" ";
-	cout<<rutas[1].ocupada<<" "<<rutas[1].fecha<<" "<<rutas[1].precio<<endl;*/
-	
 	if(opc == 1){//Si se autenticó como cliente
   		
   		//Si iniciar usuario devulve false hubo un error en el inicio de sesión,
@@ -517,7 +530,20 @@ int main(){
   		if(sesion == true){
   			string opc2;
   			for(;;){
-				consultarRuta(rutas, clienteSesion);
+  				
+  				menu2(opc3);
+  				if(opc3 == 1){
+					consultarRuta(rutas, clienteSesion);
+				}else if(opc3 == 2){
+					consultarRutaPorId(rutas);
+				}else if(opc3 == 3){
+					
+					string codigo;
+					cout<<"\n\nINGRESE EL CODIGO DE LA RUTA: ";
+					cin>>codigo;
+					
+					comprar(codigo,rutas,clienteSesion);
+				}
 				
 				cout<<"\n\n¿DESEA SALIR?\n";
 				cout<<"1. SI\n";
@@ -537,61 +563,68 @@ int main(){
 		}
   		
 	}else if(opc == 2){//Si se autenticó como administrador
-		int op=1, pos=0;
-		string id1, password1, nombre1, apellido1, linea1="";
-		while(op!=7){
-			system("cls");
-			menuAdmin();
-			cin>>op;
-			switch(op){
-				case 1:
-					system("cls");
-					cout<<"INGRESAR UN NUEVO CLIENTE"<<endl<<endl;
-					cout<<"INGRESE ID DE USUARIO: ";
-					cin>>id1;
-					do{					
-						cout<<"INGRESE PASSWORD DE USUARIO (4 DIGITOS): ";
-						cin>>password1;
-					}while(password1.size()!=4);					
-					cout<<"INGRESE NOMBRE DE USUARIO: ";
-					cin>>nombre1;
-					cout<<"INGRESE APELLIDO DE USUARIO: ";
-					cin>>apellido1;	
-					pos=contarclientes(clientes);
-					clientes[pos].Usuario=id1;				
-					clientes[pos].Password=password1;				
-					clientes[pos].Nombre=nombre1;				
-					clientes[pos].Apellido=apellido1;
-					linea1='\n'+id1+','+password1+','+nombre1+','+apellido1;			
-					archivarcliente(linea1);
-					cout<<"GUARDADO EXITOSAMENTE"<<endl;
-					system("pause");
-					break;
-				case 2:
-					system("cls");
-					cout<<"LISTADO DE CLIENTES"<<endl<<endl;
-					imprimirclientes(clientes);
-					system("pause");					
-					break;
-				case 3:
-					system("cls");
-					cout<<"BUSCAR PERSONA POR ID"<<endl<<endl;
-					cout<<"INGRESE ID A BUSCAR: ";
-					cin>>id1;
-					pos=buscarcliente(clientes, id1);
-					if(pos==-1){
-						cout<<"\n\n\t...NO EXISTE ESE CLIENTE...\n\n";
-					}else{
-						cout<<"\n\n  ID                  PASSWORD                  NOMBRE                  APELLIDO"<<endl;
-						cout<<"--------------------------------------------------------------------------------"<<endl;
-						//cout<<clientes[i].Usuario
-						cout<<clientes[pos].Usuario<<"                  "<<clientes[pos].Password<<"                  ";
-						cout<<clientes[pos].Nombre<<"                  "<<clientes[pos].Apellido<<endl<<endl<<endl;	
-					}										
-					system("pause");
-					break;
-				default: break;
+		
+		sesion = iniciarAdmin();
+		if(sesion == true){
+			int op=1, pos=0;
+			string id1, password1, nombre1, apellido1, linea1="";
+			while(op!=7){
+				system("cls");
+				menuAdmin();
+				cin>>op;
+				switch(op){
+					case 1:
+						system("cls");
+						cout<<"INGRESAR UN NUEVO CLIENTE"<<endl<<endl;
+						cout<<"INGRESE ID DE USUARIO: ";
+						cin>>id1;
+						do{					
+							cout<<"INGRESE PASSWORD DE USUARIO (4 DIGITOS): ";
+							cin>>password1;
+						}while(password1.size()!=4);					
+						cout<<"INGRESE NOMBRE DE USUARIO: ";
+						cin>>nombre1;
+						cout<<"INGRESE APELLIDO DE USUARIO: ";
+						cin>>apellido1;	
+						pos=contarclientes(clientes);
+						clientes[pos].Usuario=id1;				
+						clientes[pos].Password=password1;				
+						clientes[pos].Nombre=nombre1;				
+						clientes[pos].Apellido=apellido1;
+						linea1='\n'+id1+','+password1+','+nombre1+','+apellido1;			
+						archivarcliente(linea1);
+						cout<<"GUARDADO EXITOSAMENTE"<<endl;
+						system("pause");
+						break;
+					case 2:
+						system("cls");
+						cout<<"LISTADO DE CLIENTES"<<endl<<endl;
+						imprimirclientes(clientes);
+						system("pause");					
+						break;
+					case 3:
+						system("cls");
+						cout<<"BUSCAR PERSONA POR ID"<<endl<<endl;
+						cout<<"INGRESE ID A BUSCAR: ";
+						cin>>id1;
+						pos=buscarcliente(clientes, id1);
+						if(pos==-1){
+							cout<<"\n\n\t...NO EXISTE ESE CLIENTE...\n\n";
+						}else{
+							cout<<"\n\n  ID                  PASSWORD                  NOMBRE                  APELLIDO"<<endl;
+							cout<<"--------------------------------------------------------------------------------"<<endl;
+							//cout<<clientes[i].Usuario
+							cout<<clientes[pos].Usuario<<"                  "<<clientes[pos].Password<<"                  ";
+							cout<<clientes[pos].Nombre<<"                  "<<clientes[pos].Apellido<<endl<<endl<<endl;	
+						}										
+						system("pause");
+						break;
+					default: break;
+				}
 			}
+		}else
+		{
+			cout<<"\n\nVERIFIQUE LOS DATOS DE INICIO DE SESIÓN\n\n";
 		}
 	}else{//Opción salir
   		cout<<"::GRACIAS POR CONSULTAR NUESTRO MENU::"<<endl;// se imprime un mensaje de salida
